3cd64b14d6f2909d08bac2abae6869ba0f9cec75
Dmytro M <dmytro.minochkin@gmail.com>
2018-01-25 21:26:57 +0200

    Update shell_init.sh

--- scripts/shell_init.sh
+++ scripts/shell_init.sh
@@ -2,9 +2,20 @@
 
 sudo apt-get install build-essential dkms -y
 
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
 sudo apt-get install openjdk-8-jdk-headless nodejs maven -y
+
+sudo curl -O https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
+
+sudo tar -xvf go1.9.1.linux-amd64.tar.gz
+
+sudo mv go /usr/local
+
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile.d/go.sh
+
+echo "export GOROOT=$HOME/go" >> /etc/profile.d/go.sh
+echo "export PATH=$PATH:$GOROOT/bin" >> /etc/profile.d/go.sh
 
 sudo npm install -g @angular/cli
 
