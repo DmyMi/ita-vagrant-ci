28b293cd4abb67661d829b4164ad005bfbf30c03
Dmytro M <dmytro.minochkin@gmail.com>
2018-01-25 23:29:53 +0200

    Update build_docker.sh

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 gitlab-ci-multi-runner register -n --name "ITA Container" --registration-token $1 --url https://gitlab.com/ci --executor shell --tag-list academy,container
+
+gitlab-ci-multi-runner register -n --name "ITA Docker Executor" --registration-token $1 --url https://gitlab.com/ci --executor docker --docker-mysql latest --docker-postgres latest --tag-list academy,executor
