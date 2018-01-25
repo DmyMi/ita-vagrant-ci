a43b089a3772efc16236385cb223c67dc6a30adf
Dmytro M <dmytro.minochkin@gmail.com>
2018-01-25 23:23:08 +0200

    Update shell_init.sh

--- scripts/shell_init.sh
+++ scripts/shell_init.sh
@@ -8,20 +8,21 @@ sudo apt-get install openjdk-8-jdk-headless nodejs maven -y
 
 sudo curl -O https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz
 
-sudo tar -xvf go1.9.1.linux-amd64.tar.gz
+sudo tar -xvf go1.9.2.linux-amd64.tar.gz
 
 sudo mv go /usr/local
 
-echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile.d/go.sh
+sudo echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile.d/go.sh
 
-echo "export GOROOT=$HOME/go" >> /etc/profile.d/go.sh
-echo "export PATH=$PATH:$GOROOT/bin" >> /etc/profile.d/go.sh
+sudo echo "export GOROOT=$HOME/go" >> /etc/profile.d/go.sh
 
-sudo npm install -g @angular/cli
+sudo echo "export PATH=$PATH:$GOROOT/bin" >> /etc/profile.d/go.sh
 
 sudo npm install -g typescript
 
 sudo npm install -g webpack
+
+sudo npm install -g @angular/cli
 
 sudo apt-get autoclean -y
 
