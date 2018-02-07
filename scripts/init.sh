a107409cb223ad7d6c14b3c56ed69b30f069c4f7
Dmytro M <dmytro.minochkin@gmail.com>
2018-02-07 21:17:16 +0200

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -8,4 +8,11 @@ sudo apt-get install curl git -y
 
 curl -sSL https://packages.gitlab.com/install/repositories/runner/gitlab-ci-multi-runner/script.deb.sh | sudo bash
 
+cat > /etc/apt/preferences.d/pin-gitlab-runner.pref <<EOF
+Explanation: Prefer GitLab provided packages over the Debian native ones
+Package: gitlab-runner
+Pin: origin packages.gitlab.com
+Pin-Priority: 1001
+EOF
+
 sudo apt-get install gitlab-ci-multi-runner
